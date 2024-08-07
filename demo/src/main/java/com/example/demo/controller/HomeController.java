a4b9748a3ffd50a248b33f02b4f85c64ef79f336
Maria-Vitoria815 <vitoriamellomv028@outlook.com>
2024-08-07 16:01:13 -0300

    ATU

---
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/entrar")
+	public String entrar() {
+		return "entrar";
+	}
+	
+	@GetMapping("/cadastro")
+	public String cadastro() {
+		return "cadastro";
+	}
+	
+    @GetMapping("/quemSomos")
+	public String quemSomos() {
+		return "quemSomos";
+	}
+
+    @GetMapping("/planos")
+	public String planos() {
+		return "planos";
+	}
+
+    @GetMapping("/perfil")
+	public String perfil() {
+		return "perfil";
+	}
+
+    @GetMapping("/treinos")
+	public String treinos() {
+		return "treinos";
+	}
+
+    @GetMapping("/arquivos")
+	public String arquivos() {
+		return "arquivos";
+	}
+	
+}
