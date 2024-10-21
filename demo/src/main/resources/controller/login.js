b46fa3935d5085a27995b5ffe763b51d09499ec2
Emylouly <emilylouisemika@gmail.com>
2024-10-21 12:08:09 -0300

    Arrumado

--- demo/src/main/resources/controller/login.js
+++ demo/src/main/resources/controller/login.js
@@ -4,20 +4,23 @@ document.addEventListener("DOMContentLoaded", function() {
     errorMessageContainer.className = "error-message";
     form.appendChild(errorMessageContainer);
 
+
     form.addEventListener("submit", function(event) {
         event.preventDefault(); // Evita o envio padrão do formulário
+
 
         const emailInput = document.querySelector("input[type='email']").value;
         const passwordInput = document.querySelector("input[type='password']").value;
 
+
         // Verifica o login e a senha
         if (emailInput === "emily@gmail.com" && passwordInput === "123") {
             window.location.href = "perfil2.html"; // Redireciona para a página home
-        } 
-        
+        }
+       
         else if (emailInput === "josegomes@gmail.com" && passwordInput === "123") {
             window.location.href = "perfil.html"; // Redireciona para a página home
-        } 
+        }
         else {
             // Exibe uma mensagem de erro
             errorMessageContainer.innerHTML = `
